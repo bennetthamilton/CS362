fe895b0c263cf2d98e4c55038da47aa02347601e
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-10 12:45:06 -0800

    added station observer

--- exercises/ex5/exercise05_observer.rb
+++ exercises/ex5/exercise05_observer.rb
@@ -1,4 +1,4 @@
-# TODO: Your Name
+# Name: Bennett Hamilton
 # Exercise 5: Observer Pattern
 
 class WeatherStation
@@ -23,9 +23,17 @@ class WeatherStation
 
 end
 
-# TODO: Define a StationObserver class
-#       A StationObserver should have one
-#       method named notify.
+class StationObserver
+
+  def initialize(weather_station)
+    weather_station.add_observer(self)
+  end
+  
+  def notify(weather_station)
+    puts "The temperature is now #{weather_station.temperature}"
+  end
+  
+end
 
 weather_station = WeatherStation.new
 # TODO: Add a new observer to the weather station
