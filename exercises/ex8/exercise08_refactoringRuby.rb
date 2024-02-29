8904807a8ed0a8fbe616469a0b63ecabd3f3a174
bennetthamilton <bennetthamilton0@gmail.com>
2024-02-29 10:21:50 -0800

    created a hash of a hash to refactor further

--- exercises/ex8/exercise08_refactoringRuby.rb
+++ exercises/ex8/exercise08_refactoringRuby.rb
@@ -1,42 +1,18 @@
 # Bennett Hamilton
 # CS 362 Exercise 8: Intermediate Ruby
 
-class WordInterface
-  def initialize
-    @command_names = ["noun", "verb", "adjective", "adverb", "gerund"]
-  end
+$command_hash_names = {
+  'noun'=> {:times => 2, :type => 'Noun', :word => 'Cadillac'},
+  'verb'=> {:times => 2, :type => 'Verb', :word => 'Accelerate'},
+  'adjective'=> {:times => 3, :type => 'Adjective', :word => 'Dark'},
+  'adverb'=> {:times => 2, :type => 'Adverb', :word => 'Confidently'},
+  'gerund'=> {:times => 4, :type => 'Gerund', :word => 'Grooving'}
+}
 
-  def get_command
-    puts "Enter a command: " + @command_names.join(' ')
-    selection = gets.chomp
-    if @command_names.include?(selection)
-      send(selection)
-    else
-      puts "Invalid command"
-    end
-  end
-
-  def noun
-    2.times { puts "Noun: Cadillac" }
-  end
-
-  def verb
-    2.times { puts "Verb: Accelerate" }
-  end
-
-  def adjective
-    3.times { puts "Adjective: Dark" }
-  end
-
-  def adverb
-    2.times { puts "Adverb: Confidently" }
-  end
-
-  def gerund
-    4.times { puts "Gerund: Grooving" }
-  end
+def to_word(word)
+  $command_hash_names[word][:times].times { puts $command_hash_names[word][:type] + ": " + $command_hash_names[word][:word]}
 end
 
-word_interface = WordInterface.new
-word_interface.get_command
-
+puts "Enter a command: " + $command_hash_names.keys.join(" ")
+selection = gets.chomp
+to_word(selection)
